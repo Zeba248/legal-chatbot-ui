0761379238853d94ddbd7f36cfb5c9bf32ddfe51
Zeba248 <ridaofficial.2482@gmail.com>
2025-06-29 17:36:46 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
-  ],
-  theme: {
-    extend: {},
-  },
+  darkMode: 'class',  // 👈 add this line
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: { extend: {} },
   plugins: [],
 }
