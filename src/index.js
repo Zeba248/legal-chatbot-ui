4d874ddd3773668ef7980e7fb0fd4aa81d35f321
Zeba248 <ridaofficial.2482@gmail.com>
2025-07-05 16:05:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"; // ✅ Tailwind styles
-import App from "./App"; 
+import LegalChatbot from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
